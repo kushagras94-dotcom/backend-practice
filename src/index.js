72bd24205f2ffcb712211ae8af8bd6ebb07a3378
kushagras94-dotcom <kushagras94@gmail.com>
2026-06-18 23:54:19 +0530

    utils discussion

--- src/index.js
+++ src/index.js
@@ -11,7 +11,16 @@ dotenv.config({
 
 
 connectDB()
-
+.then(()=>{
+    application.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB connection afiled", err);
+    
+})
 
 
 
